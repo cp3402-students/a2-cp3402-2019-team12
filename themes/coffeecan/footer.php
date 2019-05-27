eda836d4b6f82d40eaeba041f9ea470806e058a1
Nick Jucker <nickolas.jucker@my.jcu.edu.au>
2019-05-27 12:49:13 +1000

    Revert "Final"
    This reverts commit 9c6ae9eaa053db0039e342f1c3ad11f588184f73.

--- themes/coffeecan/footer.php
+++ themes/coffeecan/footer.php
@@ -11,34 +11,34 @@
 
 ?>
 
-</div><!-- #content -->
+	</div><!-- #content -->
 
-<?php get_sidebar('footer'); ?>
+	<?php get_sidebar( 'footer' ); ?>
 
-<footer id="colophon" class="site-footer" role="contentinfo">
-    <div class="site-footer__wrap">
-        <?php
-        // Make sure there is a social menu to display.
-        if (has_nav_menu('social')) { ?>
-            <nav class="social-menu">
-                <?php
-                wp_nav_menu(array(
-                    'theme_location' => 'social',
-                    'menu_class' => 'social-links-menu',
-                    'depth' => 1,
-                    'link_before' => '<span class="screen-reader-text">',
-                    'link_after' => '</span>' . coffeecan_get_svg(array('icon' => 'chain')),
-                ));
-                ?>
-            </nav><!-- .social-menu -->
-        <?php } ?>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-footer__wrap">
+			<?php
+			// Make sure there is a social menu to display.
+			if ( has_nav_menu( 'social' ) ) { ?>
+			<nav class="social-menu">
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'social',
+						'menu_class'     => 'social-links-menu',
+						'depth'          => 1,
+						'link_before'    => '<span class="screen-reader-text">',
+						'link_after'     => '</span>' . coffeecan_get_svg( array( 'icon' => 'chain' ) ),
+					) );
+				?>
+			</nav><!-- .social-menu -->
+			<?php } ?>
 
-        <div class="site-info">
-            <div>The Coffee Can since May 2014</div>
-            <div><?php printf(esc_html__('Theme: %1$s by %2$s', 'coffeecan'), 'Coffeecan', 'Team 12'); ?></div>
-        </div><!-- .site-info -->
-    </div><!-- .site-footer__wrap -->
-</footer><!-- #colophon -->
+			<div class="site-info">
+                <div><a href="http://13.210.248.167" rel="designer">Opening Hours</a></div>
+				<div><?php printf( esc_html__( 'Theme: %1$s by %2$s', 'coffeecan' ), 'Coffeecan', 'Team 12' ); ?></div>
+			</div><!-- .site-info -->
+		</div><!-- .site-footer__wrap -->
+	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
